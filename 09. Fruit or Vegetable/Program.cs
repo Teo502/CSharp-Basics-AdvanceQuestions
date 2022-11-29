7b845031da55f57b1c18df14318e0384a2deed51
ShadowDog <112018746+Teo502@users.noreply.github.com>
2022-11-29 14:31:22 +0000

    Add files via upload

---
+++ 09. Fruit or Vegetable/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _09._Fruit_or_Vegetable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string food = Console.ReadLine();
+        
+            switch (food)
+            {
+                case "banana":
+                    Console.WriteLine("fruit");
+                    break;
+                case "apple":
+                    Console.WriteLine("fruit");
+                    break;
+                case "kiwi":
+                    Console.WriteLine("fruit");
+                    break;
+                case "cherry":
+                    Console.WriteLine("fruit");
+                    break;
+                case "lemon":
+                    Console.WriteLine("fruit");
+                    break;
+                case "grapes":
+                    Console.WriteLine("fruit");
+                    break;
+                case "tomato":
+                    Console.WriteLine("vegetable");
+                    break;
+                case "cucumber":
+                    Console.WriteLine("vegetable");
+                    break;
+                case "pepper":
+                    Console.WriteLine("vegetable");
+                    break;
+                case "carrot":
+                    Console.WriteLine("vegetable");
+                    break;
+                    default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+
+            
+                
+        }
+    }
+}
---
+++ 12. Trade Commissions/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace _12._Trade_Commissions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+            double commsion = 0;
+            
+            switch (town)
+            {
+                case "Sofia":
+                    if (0 <= sales && sales <= 500)
+                    {
+                        commsion = 0.05;
+                    }
+                    else if (500 < sales && sales <= 1000)
+                    {
+                        commsion = 0.07;
+                    } else if (sales > 1000 & sales <= 10000)
+                    {
+                        commsion = 0.08;
+                    } else if (sales > 10000)
+                    {
+                        commsion = 0.12;
+                    }
+                    break;
+                case "Plovdiv":
+                    if (0 <= sales && sales <= 500)
+                    {
+                        commsion = 0.055;
+                    }
+                    else if (500 < sales && sales <= 1000)
+                    {
+                        commsion = 0.08;
+                    }
+                    else if (sales > 1000 & sales <= 10000)
+                    {
+                        commsion = 0.12;
+                    }
+                    else if (sales > 10000)
+                    {
+                        commsion = 0.145;
+                    }
+                    break;
+                case "Varna":
+                    if (0 <= sales && sales <= 500)
+                    {
+                        commsion = 0.045;
+                    }
+                    else if (500 < sales && sales <= 1000)
+                    {
+                        commsion = 0.075;
+                    }
+                    else if (sales > 1000 & sales <= 10000)
+                    {
+                        commsion = 0.1;
+                    }
+                    else if (sales > 10000)
+                    {
+                        commsion = 0.13;
+                    } else
+                    {
+                        Console.WriteLine("error");
+                    }
+                    break;
+                    default:
+                    Console.WriteLine("error");
+                    break;
+
+            }
+            double totalSum = sales * commsion;
+            if (commsion > 0)
+            {
+                Console.WriteLine($"{totalSum:f2}");
+            }
+        }
+    }
+}
